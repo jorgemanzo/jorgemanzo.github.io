5a8d6814cc130a9c22c8f1a755ec33de4f9436e6
jorgemanzo <manzoj@oregonstate.edu>
2018-01-25 19:39:57 -0800

    added Ids to radial forms and testing radial form input

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 var userInput;
 var isValid;
 
+
 function convertInput() {
     userInput = document.getElementsByName("input")[0].value;
     console.log("convertInput() was called at least!")
     isValid = checkInput(userInput);
     if(isValid) {
-      alert(userInput);
+      if(document.getElementById("dec").checked){
+        console.log("You checked decimal yes?");
+      }
+      else if(document.getElementById("hex").checked){
+        console.log("You checked Hexadecimal yes?");
+      }
+      else if(document.getElementById("bi").checked){
+        console.log("You checked binary yes?");
+      }
+
     }
 }
 
