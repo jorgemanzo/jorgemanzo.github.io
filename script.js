e3a441b8e1331e1ef952933726bb1e15d417df71
jorgemanzo <manzoj@oregonstate.edu>
2018-01-25 19:29:20 -0800

    Testing how passing args works in JS

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var userInput;
+var isValid;
 
 function convertInput() {
     userInput = document.getElementsByName("input")[0].value;
     console.log("convertInput() was called at least!")
+    isValid = checkInput(userInput);
+    if(isValid) {
+      alert(userInput);
+    }
+}
+
+
+function checkInput(userInput) {
     if (userInput == "") {
         alert("Please give input!");
         return false;
     }
     else {
-      alert(userInput);
-      return  true;
+        return true;
     }
 }
