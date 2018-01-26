2e984f94257838c1d01205bcd7da90cdef48159e
jorgemanzo <manzoj@oregonstate.edu>
2018-01-25 20:14:28 -0800

    Testing converting decimal to binary

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-var userInput;
-var isValid;
-
 
 function convertInput() {
+    var userInput;
+    var isValid;
+
     userInput = document.getElementsByName("input")[0].value;
     console.log("convertInput() was called at least!")
     isValid = checkInput(userInput);
     if(isValid) {
       if(document.getElementById("dec").checked){
         console.log("You checked decimal yes?");
+        toBinary(userInput);
       }
       else if(document.getElementById("hex").checked){
         console.log("You checked Hexadecimal yes?");
@@ -18,6 +19,25 @@ function convertInput() {
       }
 
     }
+}
+
+function fromDecimal(userInput) {
+    toBinary(userInput);
+
+    //convert to hex
+}
+
+function  toBinary(userInput) {
+    var atoi;
+    var temp;
+    var decimal = [];
+    atoi = parseInt(userInput);
+    for(i = 0; i < 100; i++){
+      temp = atoi % 2; //Find remainder
+      decimal.unshift(temp); //Append remainder to array
+      atoi = Math.floor(atoi/2); //Assigning the dividend to be the quotient to be used later
+    }
+    console.log(decimal);
 }
 
 
