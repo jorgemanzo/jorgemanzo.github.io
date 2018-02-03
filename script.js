c1455a61e2faf7172d491611e5f3f728188a5f2c
jorgemanzo <manzoj@oregonstate.edu>
2018-02-03 15:47:27 -0800

    See previous commit

--- script.js
+++ script.js
@@ -100,8 +100,10 @@ function binToDec(USERINPUT){
     let n = 0;
     let length = USERINPUT.toString().length - 1;
     let string = USERINPUT.toString();
+    let remove;
     console.log(length);
-    length = length - counter(string, length);
+    remove = counter(string, length);
+    length = length - remove;
     console.log(length);
     for(i = 0, j = length; j >= 0; i++, j--){
       switch (string[i]) {
